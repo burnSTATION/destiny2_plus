719a25dbfeb02056be59ca91cb16699035628eff
Jesse Frederick <frederick.jesse@gmail.com>
2017-10-29 13:59:43 +0200

    Successful calls to bungie.net API
    Getting data out and into discord

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord
 import asyncio
-import urllib.request
-import urllib.parse
+import requests
 
 client = discord.Client()
+api_key = {'X-API-Key':'a46292e41c284dfbb9065b5864ec7ff0'}
+api_root = 'https://www.bungie.net/Platform'
 
 @client.event
 async def on_ready():
-  print('authenticating...')
-  print('authenticated as:' + client.user.name + "with ID:" + client.user.id)
+  print('Authenticating...')
+  print('Authenticated with Discord ID:' + ' ' + client.user.id)
 
 @client.event
 async def on_message(message):
@@ -16,8 +17,7 @@ async def on_message(message):
       await client.send_message(message.channel, get_dsr_data())
 
 def get_dsr_data():
-  url = 'https://jsonplaceholder.typicode.com/posts/1'
-  f = urllib.request.urlopen(url)
-  return(f.read())
+  r = requests.get(api_root + '/Destiny2/Clan/2809604/WeeklyRewardState/', headers = api_key)
+  return(r.json())
 
 client.run('MzczOTg0ODcxMjQ2MzMxOTA2.DNauaA.7949_eWFqXqx1Dfsaj0TzbaO7WI')
